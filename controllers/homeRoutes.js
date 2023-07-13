c3bd49f65e2ac795e6e72f4486bdca0544d894c8
Oliver Cray <128925359+OliverCray@users.noreply.github.com>
2023-07-13 18:08:59 +0100

    Added get route for the logged in user's profile

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -50,4 +50,24 @@ router.get('/post/:post_id', async (req, res) => {
   }
 });
 
+// Use withAuth middleware to prevent access to route
+router.get('/profile', withAuth, async (req, res) => {
+  try {
+    // Find the logged in user based on the session ID
+    const userData = await User.findByPk(req.session.user_id, {
+      attributes: { exclude: ['user_password'] },
+      include: [{ model: Post }, { model: Comment }],
+    });
+
+    const user = userData.get({ plain: true });
+
+    res.render('profile', {
+      ...user,
+      logged_in: true,
+    });
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
 module.exports = router;
