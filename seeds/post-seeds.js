8d1c679c78d031dc494432edfa39ddf4ed80cd4a
elleinadseer <126515415+elleinadseer@users.noreply.github.com>
2023-07-13 21:17:50 +0100

    databases functioning changed seed code

--- seeds/post-seeds.js
+++ seeds/post-seeds.js
@@ -1,24 +1,26 @@
 const router = require('express').Router();
 
 // Import the model
-const Post = require('../../models/Post');
+// const Post = require('../../models/Post');
 
-// const { Post } = require('../models');
+const { Post } = require('../models');
 
 const postData = [
   {
    // post_id: auto increment
-    // user_id: connect to user
+      user_id: '1',
       post_title: 'my first post',
-      post_content: 'hello!'
+      post_content: 'hello!',
       // created_at: current_timestamp
-      // likes:
+      likes: '0'
       // tag_id: .INTEGER,
   },
+  /*
   {
     post_title: 'my second post',
     post_content: 'hiya!'
   },
+  */
 ];
 
 const seedPosts = () => Post.bulkCreate(postData);
