607b0c44e3b0990ee93898f00bc39772244b19ed
Oliver Cray <128925359+OliverCray@users.noreply.github.com>
2023-07-13 16:55:32 +0100

    Added post and delete routes

--- controllers/api/postRoutes.js
+++ controllers/api/postRoutes.js
@@ -1,3 +1,34 @@
 const router = require('express').Router();
+const { Post } = require('../../models');
+const withAuth = require('../../utils/auth');
+
+router.post('/', withAuth, async (req, res) => {
+  try {
+    const postData = await Post.create({
+      ...req.body,
+      user_id: req.session.user_id,
+    });
+    res.status(200).json(postData);
+  } catch (err) {
+    res.status(400).json(err);
+  }
+});
+
+router.delete('/:id', withAuth, async (req, res) => {
+  try {
+    const postData = await Post.destroy({
+      where: {
+        post_id: req.params.post_id,
+        user_id: req.session.user_id,
+      },
+    });
+
+    !postData
+      ? res.status(404).json({ message: 'No post found with this id!' })
+      : res.status(200).json(postData);
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
 
 module.exports = router;
