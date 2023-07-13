2253aa624c4562e9bca198590579f3db7671d5bb
Oliver Cray <128925359+OliverCray@users.noreply.github.com>
2023-07-13 18:02:47 +0100

    Added get route for a specific post

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -24,4 +24,30 @@ router.get('/', async (req, res) => {
   }
 });
 
+router.get('/post/:post_id', async (req, res) => {
+  try {
+    const postData = await Post.findByPk(req.params.post_id, {
+      include: [
+        {
+          model: User,
+          attributes: ['user_name'],
+        },
+        {
+          model: Comment,
+          attributes: ['comment_text', 'created_at', 'likes'],
+        },
+      ],
+    });
+
+    const post = postData.get({ plain: true });
+
+    res.render('post', {
+      ...post,
+      logged_in: req.session.logged_in,
+    });
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
 module.exports = router;
