86aecf0728c4258c9eee03649c627428f9917e8e
Oliver Cray <128925359+OliverCray@users.noreply.github.com>
2023-07-17 21:07:03 +0100

    Fixed /login route not posting correctly form site

--- public/scripts/forms.js
+++ public/scripts/forms.js
@@ -40,11 +40,11 @@ const signupFormHandler = async (event) => {
     }
   }
 };
-/*
+
 document
   .querySelector('.login-form')
   .addEventListener('submit', loginFormHandler);
-*/
+
 document
   .querySelector('.signup-form')
   .addEventListener('submit', signupFormHandler);
