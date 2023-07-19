df81131d0b58296d141c5b79761fc95d07b9f149
elleinadseer <126515415+elleinadseer@users.noreply.github.com>
2023-07-19 23:38:21 +0100

    main fixes

--- public/scripts/homepage.js
+++ public/scripts/homepage.js
@@ -111,29 +111,6 @@ const newCommentHandler = async (event) => {
   }
 };
 
-const newCommentHandler = async (event) => {
-  event.preventDefault();
-
-  const post_id = document.querySelector('post-id').value.trim();
-  const comment_text = document.querySelector('#comment-text').value.trim();
-
-  if (post_id && comment_text) {
-    const response = await fetch(`/api/comments`, {
-      method: 'POST',
-      body: JSON.stringify({ post_id, comment_text }),
-      headers: {
-        'Content-Type': 'application/json',
-      },
-    });
-
-    if (response.ok) {
-      document.location.replace('/');
-    } else {
-      alert('Failed to create comment');
-    }
-  }
-};
-
 document
   .querySelector('#logout-button')
   .addEventListener('click', logoutHandler);
