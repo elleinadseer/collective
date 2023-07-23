816af81cda17caf507fbe1f9edf110081c37119d
elleinadseer <126515415+elleinadseer@users.noreply.github.com>
2023-07-23 20:43:35 +0100

    deleted unnecessary files and fixed trending likes issue

--- config/connection.js
+++ config/connection.js
@@ -2,7 +2,7 @@ require('dotenv').config();
 
 const Sequelize = require('sequelize');
 
-let sequelize; 
+let sequelize;
 
 if (process.env.JAWSDB_URL) {
   sequelize = new Sequelize(process.env.JAWSDB_URL);
